772593824a5e08f3a1900e9a79864e8946b6e0c6
Anshul Gupta <ganshul34@gmail.com>
2019-03-23 00:23:49 +0530

    Added Inital Linking

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Layout, Header, Drawer, Navigation, Content } from "react-mdl";
 import Main from "./components/main";
+import { Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
@@ -10,18 +11,18 @@ class App extends Component {
         <Layout>
           <Header title="Title" scroll>
             <Navigation>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
           </Header>
           <Drawer title="Title">
             <Navigation>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
-              <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
           </Drawer>
           <Content>
